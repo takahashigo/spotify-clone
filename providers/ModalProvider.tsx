2a341a7dde2f616a6aab37ff5a90f0f2d8a93e35
takahashigo <go20001104@gmail.com>
2023-06-10 12:23:24 +0900

    音楽のアップロード機能の完成

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
+import React, { useState, useEffect } from "react";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <AuthModal />
+      <UploadModal />
+    </>
+  );
+};
+
+export default ModalProvider;
